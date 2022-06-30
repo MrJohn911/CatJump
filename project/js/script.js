9fe59c7550c128fbd50048c6a86ae94106179c97
MrJohn911 <jvas@ecomp.poli.br>
2022-06-30 10:43:51 -0300

    made new changes

--- project/js/script.js
+++ project/js/script.js
@@ -1,17 +1,16 @@
-const isJumping = false;
+var isJumping = false;
 
 const cat = document.querySelector('.cat-walk');
 const wall = document.querySelector('.wall');
 
 const jump = () => {
-    
+    isJumping = true;
     cat.classList.add('jump');
 
     setTimeout(() => {
-
         cat.classList.remove('jump');
-
-    }, 800);
+        isJumping = false;
+    }, 600);
 }
 
 const loop = setInterval(() => {
@@ -19,19 +18,27 @@ const loop = setInterval(() => {
     const wallPosition = wall.offsetLeft;
     const catPosition = +window.getComputedStyle(cat).bottom.replace('px', '');
 
-    console.log(catPosition);
-
-    if(wallPosition <= 220 && wallPosition > 0 && catPosition < 10) {
+    if(wallPosition <= 220 && wallPosition > 0 && catPosition < 80) {
         wall.style.animation = 'none';
         wall.style.left = `${wallPosition}px`;
+
+        cat.style.animation = 'none';
+        cat.style.bottom = `${catPosition - 30}px`;
+        cat.src = './images/sad_cat.png';
+        cat.style.width = '150px';
+        cat.style.marginLeft = '65px';
+        
+
+        clearInterval(loop);
     }
 
 }, 10);
 
 document.body.onkeydown = function(e) {
-    if (e.key == " " ||
+    if ((e.key == " " ||
         e.code == "Space" ||      
-        e.keyCode == 32      
+        e.keyCode == 32) &&
+        !isJumping     
     ) {
       jump();
     }
